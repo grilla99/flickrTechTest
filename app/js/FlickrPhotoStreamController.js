20d0c3ef54f04d23aba14359f8cb7ed3496498e3
Grilla99 <aaron.oliver.grill@gmail.com>
2018-01-29 09:15:55 +0000

    Made alterations to variable names

--- app/js/FlickrPhotoStreamController.js
+++ app/js/FlickrPhotoStreamController.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular.module('app')
-        .component('helloWorld', {
+        .component('flickrResponse', {
             templateUrl: 'app/views/testView.html',
             controller: FlickrController
         }
